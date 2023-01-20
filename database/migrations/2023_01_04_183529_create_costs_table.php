85e988af6c241b60d8c193924afe330f9ca4e3ad
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2023-01-20 23:38:09 +0300

    Awsome Job

--- database/migrations/2023_01_04_183529_create_costs_table.php
+++ database/migrations/2023_01_04_183529_create_costs_table.php
@@ -15,11 +15,14 @@ return new class extends Migration
     {
         Schema::create('costs', function (Blueprint $table) {
             $table->id();
-            $table->string('cost_desc');
-            $table->integer('Spare_cost')->nullable();
-            $table->integer('mech_cost')->nullable();
-            $table->integer('other_cost')->nullable();
-            $table->integer('ref_no')->default(null);
+            $table->string('spare_cost_desc')->nullable();
+            $table->integer('Spare_cost')->default(0);
+            $table->integer('spare_qty')->default(1);
+            $table->string('mech_cost_desc')->nullable();
+            $table->integer('mech_cost')->default(0);
+            $table->string('other_cost_desc')->nullable();
+            $table->integer('other_cost')->default(0);
+            $table->integer('ref_no');
             $table->foreignId('requester_id')->nullable()->constrained('requesters','id');
             $table->timestamps();
         });
