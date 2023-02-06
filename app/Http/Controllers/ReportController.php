8092593ac31bca2857adcbd27d7f8f310ae121df
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2023-02-06 21:18:47 +0300

    s

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,61 +9,19 @@ use Illuminate\Http\Request;
 class ReportController extends Controller
 {
 
-    public function fuel()
+    public function index()
     {
-        // dd('data');
-        // $fuel_vcls = Vcl::get();
-        // if (count($request->all()) > 0) {        
-        //     $vcl = $request->input('vcl');
-        //     $id = $vcl;
-        //     $fuels =Vcl::where('id',$id)->with('vcl_hasmny_fuels', function($query) use ($request){
-        //         $str_date =  $request->input('start');            
-        //         $end_date = $request->input('end');
-        //         $query->whereBetween('fuel_date',[$str_date, $end_date]);
-        //         return $query;
-        //     })->get();
-        // }else{
-        //         $fuels=Vcl::with('vcl_hasmny_fuels', function($query){
-        //             $query->whereMonth('fuel_date', Carbon::now()->month)->get();
-                    
-        //         });
-        // }
-
-            
-
-        return view('admin.reports.fuel');
+        
+       return view('admin.reports.fuel',compact('fuel_vcls','fuels'));
         
     }
     
-    // public function cost($request)
-    // {
-    //     return view('admin.reports.cost');
-    // }
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('admin.reports.fuel');
-    }
+    
 
     /**
      * Show the form for creating a new resource.
